f99876c7c1719c76d61b469d9afe6bbefa712ec9
Anna Henningsen <sqrt@tradity.de>
2016-01-02 21:50:55 +0100

    Convert bus/ into npm module

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = exports = Object.assign({}, require('./lib/buscomponent.js'));
+exports.Bus = require('./lib/bus.js');
+exports.DirectTransport = require('./lib/directtransport.js');
+exports.ProcessTransport = require('./lib/processtransport.js');
